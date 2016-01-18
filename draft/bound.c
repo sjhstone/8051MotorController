5078cd20bb57c55f418d1e80e2db181bced5ca87
Roy Chan <roychan@bolshevik.local>
2016-01-18 11:15:37 +0800

    bound draft v3

--- draft/bound.c
+++ draft/bound.c
@@ -52,6 +52,11 @@ sbit Flag = P0^0;
 unsigned char CYCLE;  //Define period
 unsigned char PWM_ON ;//Define the time of high level
 
+/*
+  Switcher
+*/
+sbit Switcher = P0^2; // 0 = DC, 1 = Stepper
+
 
 // Main function
 void main( void ) {
@@ -60,56 +65,58 @@ void main( void ) {
   s_interval = 100;
   dc_interval = 20000;
   while(1) {
-    LED = 0;
-    if(Flag) {
-      PWM_ON++;
+    if (!Switcher) {
+      LED = 0;
+      if(Flag) {
+        PWM_ON++;
+      }
+      else {
+        PWM_ON--;
+      }
+      if(!PWM_ON){
+        LED=1;
+      }
+      dc_delay(dc_interval-PWM_ON);
     }
     else {
-      PWM_ON--;
-    }
-    if(!PWM_ON){
-      LED=1;
-    }
-    dc_delay(dc_interval-PWM_ON);
-
-
-    if (!ANTICLOCKWISE) {
-      Coil_A1_CCW();
-      delay(s_interval);
-      Coil_A1B1_CCW();
-      delay(s_interval);
-      Coil_B1_CCW();
-      delay(s_interval);
-      Coil_B1C1_CCW();
-      delay(s_interval);
-      Coil_C1_CCW();
-      delay(s_interval);
-      Coil_C1D1_CCW();
-      delay(s_interval);
-      Coil_D1_CCW();
-      delay(s_interval);
-      Coil_D1A1_CCW();
-      delay(s_interval);
-    }
-    else {
-      Coil_D1_CCW();
-      delay(s_interval);
-      Coil_D1C1_CCW();
-      delay(s_interval);
-      Coil_C1_CCW();
-      delay(s_interval);
-      Coil_C1B1_CCW();
-      delay(s_interval);
-      Coil_B1_CCW();
-      delay(s_interval);
-      Coil_B1A1_CCW();
-      delay(s_interval);
-      Coil_A1_CCW();
-      delay(s_interval);
-      Coil_A1D1_CCW();
-      delay(s_interval);
-      Coil_D1_CCW();
-      delay(s_interval);
+        if (!ANTICLOCKWISE) {
+        Coil_A1_CCW();
+        delay(s_interval);
+        Coil_A1B1_CCW();
+        delay(s_interval);
+        Coil_B1_CCW();
+        delay(s_interval);
+        Coil_B1C1_CCW();
+        delay(s_interval);
+        Coil_C1_CCW();
+        delay(s_interval);
+        Coil_C1D1_CCW();
+        delay(s_interval);
+        Coil_D1_CCW();
+        delay(s_interval);
+        Coil_D1A1_CCW();
+        delay(s_interval);
+      }
+      else {
+        Coil_D1_CCW();
+        delay(s_interval);
+        Coil_D1C1_CCW();
+        delay(s_interval);
+        Coil_C1_CCW();
+        delay(s_interval);
+        Coil_C1B1_CCW();
+        delay(s_interval);
+        Coil_B1_CCW();
+        delay(s_interval);
+        Coil_B1A1_CCW();
+        delay(s_interval);
+        Coil_A1_CCW();
+        delay(s_interval);
+        Coil_A1D1_CCW();
+        delay(s_interval);
+        Coil_D1_CCW();
+        delay(s_interval);
+      }
     }
   }
   while(2);
